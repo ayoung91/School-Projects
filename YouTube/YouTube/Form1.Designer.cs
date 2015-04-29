e09ec0d57aa68a157419d2d42c4f761e5134ac82
Adam Young <adam.youngn91@gmail.com>
2015-04-28 22:49:35 -0400

    YouTube Downloader
    You type a URL from YouTube and it grabs the data from the video which
    displays the name of the video and the video quality options. Then you
    can download the video to your desktop. Created in my C#/.NET class in
    college.

---
+++ YouTube/YouTube/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace YouTube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxQuality = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(85, 30);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(379, 20);
+            this.txtURL.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "YouTube Video Title:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(160, 68);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(304, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(505, 28);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(111, 23);
+            this.btnGet.TabIndex = 4;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Quality:";
+            // 
+            // cbxQuality
+            // 
+            this.cbxQuality.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxQuality.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbxQuality.FormattingEnabled = true;
+            this.cbxQuality.Location = new System.Drawing.Point(315, 107);
+            this.cbxQuality.Name = "cbxQuality";
+            this.cbxQuality.Size = new System.Drawing.Size(149, 21);
+            this.cbxQuality.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(50, 156);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(566, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Enabled = false;
+            this.btnDownload.Location = new System.Drawing.Point(505, 105);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(111, 23);
+            this.btnDownload.TabIndex = 8;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(294, 207);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 9;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 267);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cbxQuality);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "YouTube Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxQuality;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnOK;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ YouTube/YouTube/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+using YouTube_Downloader;
+
+
+namespace YouTube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void btnGet_Click(object sender, EventArgs e)
+        {
+            string url = this.txtURL.Text;
+            string[] videoURLs;
+            
+            try
+            {
+                videoURLs = new string[1];
+                videoURLs[0] = txtURL.Text;
+//                List<YouTubeVideoQuality> urls = YouTubeVideoDownloader.GetYouTubeVideoUrls(videoURLs);
+                List<YouTube_Downloader.YouTubeVideoQuality> urls = YouTubeDownloader.GetYouTubeVideoUrls(videoURLs);
+                
+                cbxQuality.Items.Clear();
+
+                foreach (var items in urls)
+                {
+                    cbxQuality.Items.Add(items);
+                    string videoTitle = items.VideoTitle;
+                    txtTitle.Text = videoTitle;
+
+                    
+                }
+                this.btnDownload.Enabled = true;
+
+            }
+            catch
+            {
+                this.btnDownload.Enabled = true;
+                MessageBox.Show("Load Failed!");
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            YouTubeVideoQuality tempItem = (YouTubeVideoQuality)cbxQuality.SelectedItem;
+            saveFileDialog1.FileName = tempItem.VideoTitle +"."+ tempItem.Extention;
+            saveFileDialog1.ShowDialog();
+            string folder = Path.GetDirectoryName(saveFileDialog1.FileName);
+            string file = Path.GetFileName(saveFileDialog1.FileName);
+            FileDownloader downloader = new FileDownloader(tempItem.DownloadUrl, folder, file);
+            downloader.ProgressChanged += this.OnRunWorkerProgress;
+            downloader.RunWorkerCompleted += OnRunWorkerCompleted;
+            downloader.RunWorkerAsync();
+        }
+
+
+        private void OnRunWorkerCompleted(Object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Completed");
+        }
+
+        private void OnRunWorkerProgress(Object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+
+    }
+}
